e3438fd87c038aa060f076422011321b6eaee47b
LilMGenius <smsmeee@naver.com>
2025-01-03 09:47:04 +0900

    feat: tickets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database/database.module";
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, TicketsModule],
   controllers: [],
   providers: [],
 })
