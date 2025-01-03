2724bd56bd7744bede04ad84481330a9743dd7e7
LilMGenius <smsmeee@naver.com>
2025-01-03 09:50:35 +0900

    feat: payment

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database/database.module";
 import { TicketsModule } from './tickets/tickets.module';
 import { BalanceModule } from "./balance/balance.module";
+import { PaymentModule } from "./payment/payment.module";
 
 @Module({
-  imports: [DatabaseModule, TicketsModule, BalanceModule],
+  imports: [DatabaseModule, TicketsModule, BalanceModule, PaymentModule],
   controllers: [],
   providers: [],
 })
