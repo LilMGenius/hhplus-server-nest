1398ef35e113825c8d87c4077ab6edb2487320b5
LilMGenius <smsmeee@naver.com>
2025-01-10 01:43:29 +0900

    chore: use validation-pipe, swagger docs config, listen callback log

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appPort = process.env.PORT ?? 3000;
+  
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Concert Ticketing API')
-    .setDescription('API Docs for Concert Ticketing Server')
-    .setVersion('1.0')
-    .addBearerAuth() // JWT Authorization
+    .setVersion('0.1')
+    .setDescription('Auto-generated API docs by Swagger for `Concert Ticketing Server`')
+    .setContact('@LilMGenius', 'https://github.com/LilMGenius', 'smsmeee@naver.com')
+    // .addServer(`http://localhost:${appPort}`)
     .build();
 
   const document = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(appPort, () => {
+    console.log(`Server is running on http://localhost:${appPort}`);
+  });
 }
 bootstrap();
